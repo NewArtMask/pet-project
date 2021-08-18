0ceac1760a892e51156b83996c23a3161f0e6869
Oleg Postolovskyi <newartmask@gmail.com>
2021-08-18 20:08:27 +0300

    Created page user and when the user clicks on item on users page it gets to the item page

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -6,20 +6,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UsersService {
   apiURL = 'https://the-one-api.dev/v2';
-  // api tocken: sUxFhZvnkYN1MAyx6dpe
   // apiURL = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) { }
 
   getUsers(offset: number) {
-    return this.http.get(`${this.apiURL}/character?offset=${offset}&limit=55`, {
+    return this.http.get(`${this.apiURL}/character?offset=${offset}&limit=5`, {
       headers: {
         'Authorization': 'Bearer sUxFhZvnkYN1MAyx6dpe'
       }
     });
   }
   //'/features/:id/:username'
-  getUser() {
-    return this.http.get(`${this.apiURL}`);
+  getUser(id: string) {
+    return this.http.get(`${this.apiURL}/character/${id}`, {
+      headers: {
+        'Authorization': 'Bearer sUxFhZvnkYN1MAyx6dpe'
+      }
+    });
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../service/users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  user: any;
+  id!: string;
 
-  constructor() { }
+  constructor(private usersService: UsersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+
+      this.usersService.getUser(this.id)
+        .subscribe(response => {
+          this.user = JSON.parse(JSON.stringify(response)).docs[0];
+        });
+    });
   }
 
 }
